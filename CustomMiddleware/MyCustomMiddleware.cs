8cdb4feba6d7a2826a9a521bbeb2105f0f7269cf
Zack Spring Chief <jzacksc@gmail.com>
2024-03-19 17:05:14 -0600

    making custom middleware

--- CustomMiddleware/MyCustomMiddleware.cs
+++ CustomMiddleware/MyCustomMiddleware.cs
@@ -5,9 +5,9 @@ public class MyCustomMiddleware : IMiddleware
 {
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
-        await context.Response.WriteAsync("My Custom Middleware --- Starts");
+        await context.Response.WriteAsync("My Custom Middleware --- Starts\n");
         await next(context);
-        await context.Response.WriteAsync("My Custom Middleware --- Ends");
+        await context.Response.WriteAsync("My Custom Middleware --- Ends\n");
     }
 }
 public static class CustomMiddlewareExtention
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 
 
 using MiddlewareExample.CustomMiddleware;
+using refreashOnCsharp.CustomMiddlewar;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<MyCustomMiddleware>();
@@ -9,14 +10,15 @@ var app = builder.Build();
 
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("Hello");
+    await context.Response.WriteAsync("Hello\n");
     await next(context);
 });
 
-app.UseMyCustomMiddleware();
+//app.UseMyCustomMiddleware();
+app.UseHelloCustomMiddleware();
 
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("Hello again again");
+    await context.Response.WriteAsync("Hello again again\n");
 });
 app.Run();
