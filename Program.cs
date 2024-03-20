3c06103867028a0405fd2067aa1a7f34a05d941e
Zack Spring Chief <jzacksc@gmail.com>
2024-03-19 21:43:46 -0600

    optional params

--- Program.cs
+++ Program.cs
@@ -16,17 +16,24 @@ app.UseEndpoints(endpoints =>
         await context.Response.WriteAsync($"In files --- filename: {filename} --- extension: {extension}");
     });
 
-    endpoints.Map("employee/profile/{EmployeeName=Kylie}", async
+    endpoints.Map("employee/profile/{EmployeeName=KylieIsACutie}", async
         context =>
         {
             string? employeeName = Convert.ToString(context.Request.RouteValues["employeename"]);
             await context.Response.WriteAsync($"In Employee profile: {employeeName}");
         });
 
-    endpoints.Map("products/details/{id=7}", async context =>
+    endpoints.Map("products/details/{id?}", async context =>
     {
-        int id = Convert.ToInt32(context.Request.RouteValues["id"]);
-        await context.Response.WriteAsync($"Product details: {id}");
+        if (context.Request.RouteValues.ContainsKey("id"))
+        {
+            int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+            await context.Response.WriteAsync($"Product details: {id}");
+        }
+        else
+        {
+            await context.Response.WriteAsync($"Product details: id is not supplied");
+        }
     });
 
 });
