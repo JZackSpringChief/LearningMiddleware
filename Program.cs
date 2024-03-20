dba14f1110b60f7631a0185e066b170341b290b6
Zack Spring Chief <jzacksc@gmail.com>
2024-03-19 21:04:19 -0600

    GetEndPoint

--- Program.cs
+++ Program.cs
@@ -1,8 +1,28 @@
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    Endpoint? endpoint = context.GetEndpoint();
+    if (endpoint != null)
+    {
+        await context.Response.WriteAsync($"Endpoint: {endpoint.DisplayName}\n");
+    }
+    await next(context);
+});
 //enabled routing
 app.UseRouting();
+
+app.Use(async (context, next) =>
+{
+    Endpoint? endpoint = context.GetEndpoint();
+    if (endpoint != null)
+    {
+        await context.Response.WriteAsync($"Endpoint: {endpoint.DisplayName}\n");
+    }
+    await next(context);
+});
 //created endpoints
 app.UseEndpoints(endpoints =>
 {
