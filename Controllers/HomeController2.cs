db018611ede52b1f9f4fddec427686eb5aefdd03
Zack Spring Chief <jzacksc@gmail.com>
2024-03-21 19:16:37 -0600

    adding status codes

--- Controllers/HomeController2.cs
+++ Controllers/HomeController2.cs
@@ -12,29 +12,26 @@ public class HomeController2 : Controller
     {
         if (!Request.Query.ContainsKey("bookid"))
         {
-            Response.StatusCode = 400;
-            return Content("book id is not supplied");
+            return BadRequest("book id is not supplied");
         }
         if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
         {
-            Response.StatusCode = 400;
-            return Content("Book id can't be null or empty");
+            return BadRequest("Book id can't be null or empty");
+
         }
         int bookId = Convert.ToInt16(ControllerContext.HttpContext.Request.Query["bookid"]);
         if (bookId <= 0)
         {
-            Response.StatusCode = 400;
-            return Content("Book id can't be less than 0");
+            return BadRequest("Book id can't be less than 0");
         }
         if (bookId > 1000)
         {
 
-            return Content("Book id cannot be greater than 1000");
+            return NotFound("Book id cannot be greater than 1000");
         }
         if (Convert.ToBoolean(Request.Query["isloggedin"]) == false)
         {
-            Response.StatusCode = 401;
-            return Content("User is not logged in");
+            return Unauthorized("User is not logged in");
         }
         return Content("Kylie's a Cutie!!!");
     }
