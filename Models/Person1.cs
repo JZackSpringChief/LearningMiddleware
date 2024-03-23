6574cc84bbfce9d45b186e7c8b67c812400ee415
Zack Spring Chief <jzacksc@gmail.com>
2024-03-23 15:00:53 -0600

    IValidatable

--- Models/Person1.cs
+++ Models/Person1.cs
@@ -4,12 +4,12 @@ using refreashOnCsharp.CustomValidators;
 
 namespace refreashOnCsharp.Models;
 
-public class Person1
+public class Person1 : IValidatableObject
 {
     [Required(ErrorMessage = "{0} Cant be null or empty")]
     [Display(Name = "Person Name")]
     [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long")]
-    [RegularExpression("^[A-Za-z .]$", ErrorMessage = "{0} should contain only alphabets, space and dot (.)")]
+    [RegularExpression("^[A-Za-z .]*$", ErrorMessage = "{0} should contain only alphabets, space and dot (.)")]
     public string? PersonName { get; set; }
     [Required(ErrorMessage = "{0} cannot be blank")]
     [EmailAddress(ErrorMessage = "{0} should be a proper email address")]
@@ -32,8 +32,18 @@ public class Person1
     public DateTime? FromDate { get; set; }
     [DateRangeValidator("FromDate", ErrorMessage = "'From Date' should be older than or equal to 'to date")]
     public DateTime? ToDate { get; set; }
+    public int? Age { get; set; }
     public override string ToString()
     {
         return $"Person Object - - - Person Name: {PersonName}, Email: {Email}, Phone: {Phone}, Password: {Password}, Confirm Password: {ConfirmPassword}, Price: {Price} ";
+    }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (DateOfBirth.HasValue == false && Age.HasValue == false)
+        {
+            yield return new ValidationResult("Either DoB or age must be supplied", new[] { nameof(Age) });
+        }
+
     }
 }
