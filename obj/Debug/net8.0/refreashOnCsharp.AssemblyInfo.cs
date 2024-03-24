0051760aceb9ecb875a5dc78aa72065453b613ab
Zack Spring Chief <jzacksc@gmail.com>
2024-03-23 21:52:56 -0600

    rzr 4 loop

--- obj/Debug/net8.0/refreashOnCsharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/refreashOnCsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("refreashOnCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd8c873bc28f331d77d10a5596602b58f74847d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ff84c50fa55417b29e85a31350f6705637ef52")]
 [assembly: System.Reflection.AssemblyProductAttribute("refreashOnCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("refreashOnCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
